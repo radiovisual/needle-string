ddef61c9777810f3ff4b1f4f24901369fb71465a
Michael Wuergler <wuergler@gmail.com>
2016-02-07 17:52:07 +0000

    initial commit

---
+++ test.js
@@ -0,0 +1,26 @@
+import test from 'ava';
+import fn from './';
+
+test('expects a haystack string', t => {
+	t.throws(() => {
+		fn({}, 'unicorns');
+	});
+});
+
+test('expects a needle string', t => {
+	t.throws(() => {
+		fn('unicorns', {});
+	});
+});
+
+test('finds needles in a haystack', t => {
+	t.is(fn('the magical unicorn is magic', 'magic'), 2);
+	t.is(fn('@@@@@@@@@@', '@'), 10);
+	t.is(fn('@x@Y@x@', 'Y'), 1);
+	t.is(fn('@x@Y@x@', 'x'), 2);
+	t.is(fn('../../../../../../', '../'), 6);
+});
+
+test('returns a number', t => {
+	t.is(typeof fn('yo', 'yo'), 'number');
+});
